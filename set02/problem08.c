552c572331118893c957e734efbbfa4bf34d71c4
prathana1401 <144999648+prathana1401@users.noreply.github.com>
2023-10-30 08:54:50 +0530

    committed 10/30/2023, 8:54:49 AM

--- set02/problem08.c
+++ set02/problem08.c
@@ -13,7 +13,13 @@ Triangle find_smallest_triangle(int n, Triangle t[n]);
 void output(int n, Triangle t[n], Triangle smallest);
 int main()
 {
-
+    int n = input_n();
+    Triangle t[n];
+    input_n_triangles(n, t);
+    find_n_areas(n, t);
+    Triangle smallest = find_smallest_triangle(n, t);
+    output(n, t, smallest);
+    return 0;
 }
 int input_n()
 {
@@ -33,6 +39,46 @@ Triangle input_triangle()
 }
 void input_n_triangles(int n, Triangle t[n])
 {
-    int t[n];
-    printf("enter the ")
+    for (int i = 0; i < n; i++) {
+        printf("Triangle %d:\n", i + 1);
+        t[i] = input_triangle();
+    }
 }
+void find_area(Triangle *t)
+{
+    t->area = 0.5 * t->base * t->altitude;
+}
+void find_n_areas(int n, Triangle t[n])
+    {
+    for (int i = 0; i < n; i++) {
+        find_area(&t[i]);
+    }
+}
+Triangle find_smallest_triangle(int n, Triangle t[n])
+{
+    Triangle smallest = t[0];
+    for (int i = 1; i < n; i++) {
+        if (t[i].area < smallest.area) {
+            smallest = t[i];
+        }
+    }
+    return smallest;
+}
+void output(int n, Triangle t[n], Triangle smallest) 
+{
+    printf("Triangle areas:\n");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("Triangle %d: Area = %.2f\n", i + 1, t[i].area);
+    }
+    printf("Smallest Triangle: Base = %.2f, Altitude = %.2f, Area = %.2f\n", smallest.base, smallest.altitude, smallest.area);
+}
+
+
+
+
+
+
+
+
+
