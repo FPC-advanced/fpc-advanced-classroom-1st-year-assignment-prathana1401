8257281629f2d7ca23be8e2ec8d6d2eba452b1fe
prathana1401 <144999648+prathana1401@users.noreply.github.com>
2023-11-17 09:21:51 +0530

    committed 11/17/2023, 9:21:50 AM

---
+++ set04/problem02.c
@@ -0,0 +1,34 @@
+//Write a program to find the smallest of three fractions
+#include <stdio.h>
+typedef struct {
+    int num, den;
+} Fraction;
+void input(Fraction *frac1, Fraction *frac2, Fraction *frac3);
+Fraction find_smallest(Fraction frac1, Fraction frac2, Fraction frac3);
+void output(Fraction smallest);
+int main()
+{
+
+}
+void input(Fraction *frac1, Fraction *frac2, Fraction *frac3)
+{
+    printf("enter the first fraction(numerator and denominator):\n");
+    scanf("%d %d",&frac1->num, &frac1->den);
+     printf("enter the second fraction(numerator and denominator):\n");
+    scanf("%d %d",&frac2->num, &frac2->den);
+     printf("enter the third fraction(numerator and denominator):\n");
+    scanf("%d %d",&frac3->num, &frac3->den);
+}
+Fraction find_smallest(Fraction frac1, Fraction frac2, Fraction frac3)
+{
+    Fraction smallest = frac1;
+    if ((frac2.num * smallest.den) < (smallest.num * frac2.den))
+        smallest = frac2;
+    if ((frac3.num * smallest.den) < (smallest.num * frac3.den))
+        smallest = frac3;
+    return smallest;
+}
+void output(Fraction smallest) {
+    printf("The smallest of %d/%d, %d/%d, and %d/%d is %d/%d\n", frac1.num, frac1.den, frac2.num, frac2.den, frac3.num, frac3.den,
+           smallest.num, smallest.den);
+}
