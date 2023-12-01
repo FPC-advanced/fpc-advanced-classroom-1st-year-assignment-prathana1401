28a71ff7f261f1027a4894a64062b4e47c79011f
prathana1401 <144999648+prathana1401@users.noreply.github.com>
2023-12-01 10:01:15 +0530

    committed 12/1/2023, 10:01:15 AM

--- set04/problem04.c
+++ set04/problem04.c
@@ -1,11 +1,54 @@
 //Write a program to evaluate a polynomial at a given point using [Horner's Method]
 #include <stdio.h>
+#include <stdio.h>
 int input_degree();
 void input_coefficients(int n, float a[n]);
 float input_x();
 float evaluate_polynomial(int n, float a[n], float x);
 void output(int n, float a[n], float x, float result);
-int main()
-{
+int main() {
+    int n = input_degree();
+    float coefficients[n];
+    input_coefficients(n, coefficients);
+    float x = input_x();
+    float result = evaluate_polynomial(n, coefficients, x);
+    output(n, coefficients, x, result);
+    return 0;
+}
+int input_degree() {
+    int n;
+    printf("Enter the degree of the polynomial: ");
+    scanf("%d", &n);
+    return n;
+}
+void input_coefficients(int n, float a[n]) {
+    printf("Enter the coefficients of the polynomial (from highest degree to constant term):\n");
+    for (int i = n; i >= 0; --i) {
+        scanf("%f", &a[i]);
+    }
+}
+float input_x() {
+    float x;
+    printf("Enter the value of x: ");
+    scanf("%f", &x);
+    return x;
+}
+float evaluate_polynomial(int n, float a[n], float x) {
+    float result = a[0]; 
+    for (int i = 1; i <= n; ++i) {
+        result = result * x + a[i];
+    }
+    return result;
+}
+void output(int n, float a[n], float x, float result) {
+    printf("H(%d, %.2f, %.2f) = ", n, x, x);
     
+    for (int i = 0; i <= n; ++i) {
+        if (i == 0) {
+            printf("%.2f", a[i]);
+        } else {
+            printf(" + %.2f * %.2f^%d", a[i], x, i);
+        }
+    }
+    printf(" = %.7f\n", result);
 }
---
+++ set04/problem05.c
@@ -0,0 +1,47 @@
+//Write a program to find the index of the largest number in an array
+#include <stdio.h>
+int input_size();
+void input_array(int n, int a[n]);
+int find_largest_index(int n, int a[n]);
+void output(int index);
+int main()
+{
+int n;
+n=input_size();
+int a[n];
+input_array(n,a);
+int index=find_largest_index(n,a);
+output(index);
+return 0;
+}
+int input_size()
+{
+    int n;
+    printf("enter the size of an array:\n");
+    scanf("%d",&n);
+    return n;
+}
+void input_array(int n, int a[n])
+{
+    printf("enter the elements of the array:\n");
+    for(int i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+}
+int find_largest_index(int n, int a[n])
+{
+    int index=0;
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]>index)
+        {
+            index=i;
+        }
+    }
+    return index;
+}
+void output(int index)
+{
+    printf("The index of the largest number in the array is:%d\n",index);
+}
