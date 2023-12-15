6d9bcf3dfbb6d71779437bc94e95cf69044e5258
prathana1401 <144999648+prathana1401@users.noreply.github.com>
2023-12-15 12:39:06 +0530

    committed 12/15/2023, 12:39:05 PM

--- set03/problem02.c
+++ set03/problem02.c
@@ -37,7 +37,7 @@ int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 }
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result)
 {
-    if(result)
+    if(result==1)
     {
         printf("The points (%f,%f), (%f,%f) and (%f,%f) form a triangle",x1,y1,x2,y2,x3,y3,result);
     }
